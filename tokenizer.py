f7a124e658d51d8152841c41d9296e31efc92384
Firework <firework@localhost.localdomain>
2023-07-24 17:31:02 +0900

    Split dataset into Q and A.

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,9 @@
 import sentencepiece as spm
 import pandas as pd
 
-dataset = 'dataset/ChatbotData.csv' # Dataset location
-chatbot_data = pd.read_csv(dataset, sep=',')
+dataset = 'dataset/' # Dataset location
+chatbot_data = pd.read_csv(dataset+'ChatbotData.csv', sep=',')
+chatbot_data['Q'].to_csv(dataset+'Q.txt',index=False,header=False)
+chatbot_data['A'].to_csv(dataset+'A.txt',index=False,header=False)
+
+spm
